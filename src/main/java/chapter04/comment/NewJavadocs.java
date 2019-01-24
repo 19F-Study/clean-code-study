71f23b89d1c6d1e76b222b06326f9baf988992d5
heize <syhwang@coupang.com>
2019-01-24 23:52:09 +0900

    Chapter04_comment

---
+++ src/main/java/chapter04/comment/NewJavadocs.java
@@ -0,0 +1,17 @@
+package src.main.java.chapter04.comment;
+
+/**
+ * This is NewJavadocs Class
+ *
+ * @author Heize
+ */
+public class NewJavadocs {
+
+	/**
+	 * say hello
+	 * @param name
+	 */
+	public void sayHelloToYou(String name) {
+		System.out.println(String.format("Hello Javadocs, Welcome %s", name));
+	}
+}
---
+++ src/main/java/chapter04/comment/Test.java
@@ -0,0 +1,11 @@
+package src.main.java.chapter04.comment;
+
+/**
+ * Created by Hieze on 2019. 1. 24.
+ */
+public class Test {
+	public static void main(String[] args) {
+
+		// TODO
+	}
+}
